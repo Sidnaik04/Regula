302573fabe7e22b8d2b0bde3f1a5c5247abb4051
Sidnaik04 <naiksidhant04@gmail.com>
2024-12-08 09:14:35 +0530

    Done with Weathery app. Add to codebase

--- src/Weathery/script.js
+++ src/Weathery/script.js
@@ -1,3 +1,57 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const weatherBtn = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityName = document.getElementById("city-name");
+  const cityTemperature = document.getElementById("temperature");
+  const cityDescription = document.getElementById("description");
+  const errorMssg = document.getElementById("error-message");
+
+  const API_KEY = "11ed3f246f5b55e31bb5bd088918df24";
+
+  weatherBtn.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+
+    if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    // console.log(typeof response);
+    // console.log('response: ', response);
+
+    if (!response.ok) {
+      throw new Error("City not found!!!");
+    }
+
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(data) {
+    // console.log(data);
+
+    const { name, weather, main } = data;
+
+    cityName.textContent = name;
+    cityTemperature.textContent = `Temprature: ${main.temp}`;
+    cityDescription.textContent = `Weather: ${weather[0].description}`;
+
+    weatherInfo.classList.remove("hidden");
+    errorMssg.classList.add("hidden");
+  }
+
+  function showError() {
+    weatherInfo.classList.add("hidden");
+    errorMssg.classList.remove("hidden");
+  }
+});
